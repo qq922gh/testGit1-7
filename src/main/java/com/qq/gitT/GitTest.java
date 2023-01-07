625c7227dccbf89616a3d2b8e128ececbf88fb65
PPPC <PPPC@qq.com>
2023-01-07 10:25:15 +0800

    first

---
+++ src/main/java/com/qq/gitT/GitTest.java
@@ -0,0 +1,13 @@
+package com.qq.gitT;
+
+/**
+ * @author yqq
+ * @Description
+ * @create 2023-01-07 10:16
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
